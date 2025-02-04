a81b3f12b8b3b29d6cb5f428412b5a852e1f9d91
izyryanova <irina.s.zyryanova@gmail.com>
2025-02-04 12:44:18 +0400

    Fix: assets moved to public folder

--- src/components/nftpage/NFTPage.tsx
+++ src/components/nftpage/NFTPage.tsx
@@ -35,7 +35,7 @@ const NFTPage = () => {
 
 					<div className={styles.main_container}>
 						<div className={styles.img_wrapper}>
-						<img src="/main_img.png" alt="nft_img" className={styles.img} />
+						<img src="./assets/img/main_img.png" alt="nft_img" className={styles.img} />
 						</div>
 
 						<div className={styles.container_element}>
